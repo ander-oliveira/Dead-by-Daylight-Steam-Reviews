7598412cf12d0b7c4ffe700af36813a508c39a0d
Anderson Oliveira <ander.oliveira1@icloud.com>
2022-11-13 19:07:26 -0300

    Starting Repository

---
+++ README.md
@@ -0,0 +1,11 @@
+# Dead by Daylight Steam Reviews
+
+This repository contains *438,591* **Dead by Daylight** game reviews on **Steam**.
+The aim was to process the various files, each with 100 review records in a single file (`all_dbd_reviews.json`) to later be processed in a Pandas DataFrame for a **Data Science Project**.
+
+
+## Original Repository
+
+The original data repository can be found at [Steam Store Apps Reviews](https://www.kaggle.com/datasets/souyama/steam-reviews).
+
+This repository was updated 3 months after this processing took place (in *August 2022*) and contains **45.52GB** of review data for all games on Steam.
---
+++ main.py
@@ -0,0 +1,31 @@
+from glob import glob
+import json
+
+
+def process_all_data(path, all_data = []):
+    for file in glob(path):
+        with open(file, encoding='latin-1') as json_file:    
+            data = json.load(json_file)
+            for review in data['reviews']:
+                aux = {}
+                for k,v in review.items():
+                    if k == 'author':
+                        for a, b in v.items():
+                            aux[a] = b
+                    else:
+                        aux[k] = v
+                all_data.append(aux)
+    return all_data
+
+
+def write_to_json(data, file_output):
+    with open(file_output, "w", encoding='latin-1') as outfile:
+        json.dump(data, outfile, ensure_ascii=False, indent=4)
+    
+
+
+if __name__ == "__main__":
+    path_to_data = 'data/*.json'
+    outfile = 'all_dbd_reviews.json'
+    data = process_all_data(path_to_data)
+    write_to_json(data, outfile)
